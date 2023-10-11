bf5e7c84fe509947f3d50f260566d4a456392329
naverolek2 <sebaskiba11@gmail.com>
2023-10-11 13:36:07 +0200

    bare minimum kodu

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { MongoClient } = require('mongodb');
+const mongoString = "mongodb+srv://sebaskiba11:1234@pierwszadbmongo.kywosci.mongodb.net/?retryWrites=true&w=majority"
 
+const client = new MongoClient(mongoString);
 async function main() {
- }
+await client.connect();
 
-const uri = "mongodb+srv://sebaskiba11:1234@pierwszadbmongo.kywosci.mongodb.net/?retryWrites=true&w=majority"
-const client = new MongoClient(uri);
+
+
+await client.close();
+}
+
+
+main();
