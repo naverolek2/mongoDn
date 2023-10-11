a768bbf1f31e464bb246080776bc915d5b9aa010
naverolek2 <sebaskiba11@gmail.com>
2023-10-11 13:16:35 +0200

    Pierwszy comit

---
+++ index.js
@@ -0,0 +1,7 @@
+const { MongoClient } = require('mongodb');
+
+async function main() {
+ }
+
+const uri = "mongodb+srv://sebaskiba11:1234@pierwszadbmongo.kywosci.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(uri);
