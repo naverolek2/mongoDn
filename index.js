456e5e57ad563f20e42dca79d87393c868690df6
naverolek2 <sebaskiba11@gmail.com>
2023-11-08 13:05:42 +0100

    h

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 const { MongoClient } = require('mongodb');
-const mongoString = "mongodb+srv://sebaskiba11:1234@pierwszadbmongo.kywosci.mongodb.net/?retryWrites=true&w=majority"
+const mongoString = "mongodb+srv://sebaskiba11:1234@pierwszadbmongo.kywosci.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(mongoString);
 async function main() {
